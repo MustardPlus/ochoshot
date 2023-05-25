bc95d94ab74ff21a1cf85faabe90a422ebfd4462
MustardPlus <mustardplusminus@gmail.com>
2023-05-24 22:36:30 -0700

    header images update

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Footer = () => {
     return (
         <div id="footer" className="sm:text-2xl text-base">
-            <Link href="mailto: contact@ochoshot.com"  className="text-base">contact@ochoshot.com</Link><br/>
+            <Link href="mailto: contact@ochoshot.com"  className="text-base cP">contact@ochoshot.com</Link><br/>
             © 2023 romeo
             
         </div>
